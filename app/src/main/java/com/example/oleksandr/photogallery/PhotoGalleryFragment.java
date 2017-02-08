0ace902c9f472209654827f3a4d84924cede2042
Demandr <deman13108>
2017-02-08 20:48:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/oleksandr/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,39 @@
+package com.example.oleksandr.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Oleksandr on 08.02.2017.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    public static Fragment newInstance(){
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/oleksandr/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.example.oleksandr.photogallery;
+
+//import android.app.Fragment;
+//import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Oleksandr on 24.11.2016.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
+    }
+}
