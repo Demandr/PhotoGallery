f56b529039576e294d94a2eb167cb85f1f2ee009
Demandr <deman13108>
2017-02-17 22:40:03 +0200

    load picture

---
+++ app/src/main/java/com/example/oleksandr/photogallery/FlickrFetcher.java
@@ -0,0 +1,125 @@
+package com.example.oleksandr.photogallery;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.in;
+
+/**
+ * Created by Oleksandr on 08.02.2017.
+ */
+
+public class FlickrFetcher {
+    private static final String TAG = "FlickrFetcher";
+    private static final String API_KEY = "defa67e9e6141a310a91a7e616020284";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri.parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
+
+
+    public byte[] getUrlBytes(String urlSpec)throws IOException{
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try{
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if(connection.getResponseCode() != HttpURLConnection.HTTP_OK){
+                throw  new IOException(connection.getResponseMessage() + ": with " + urlSpec);
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0){
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        }finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws  IOException{
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    private List<GalleryItem> downloadGallryItems(String url){
+        List<GalleryItem> items = new ArrayList<>();
+        try {
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON" + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items,jsonBody);
+        }catch (JSONException je){
+            Log.e(TAG, "Failed to parse JSON", je);
+        }
+        catch (IOException ioe){
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
+        return items;
+    }
+
+    public List<GalleryItem> fetchRecentPhotos(String page){
+        String url = buildUrl(FETCH_RECENTS_METHOD, page);
+        return downloadGallryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(String query){
+        String url = buildUrl(SEARCH_METHOD, query);
+        return downloadGallryItems(url);
+    }
+
+    private String buildUrl(String method, String query){
+        Uri.Builder builder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method);
+        if (method.equals(FETCH_RECENTS_METHOD)){
+            builder.appendQueryParameter("page", query);
+        }
+        if (method.equals(SEARCH_METHOD)){
+            builder.appendQueryParameter("text", query);
+        }
+        return  builder.build().toString();
+    }
+
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException{
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++){
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s")){
+                continue;
+            }
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
+        }
+
+    }
+}
--- app/src/main/java/com/example/oleksandr/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/oleksandr/photogallery/PhotoGalleryFragment.java
@@ -1,22 +1,44 @@
 package com.example.oleksandr.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Oleksandr on 08.02.2017.
  */
 
 public class PhotoGalleryFragment extends Fragment {
-    private RecyclerView mRecyclerView;
+    private static final String TAG = "PhotoGalleryFragment";
 
-    public static Fragment newInstance(){
+    private RecyclerView mRecyclerView;
+    private GridLayoutManager mGridLayoutManager;
+    private List<GalleryItem> mItems = new ArrayList<>();
+    private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
+    private int mPage = 0;
+
+    public static Fragment newInstance() {
         return new PhotoGalleryFragment();
     }
 
@@ -24,6 +46,21 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        setHasOptionsMenu(true);
+        updateItems();
+
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloaderListner(new ThumbnailDownloader.ThumbnailDownloaderListener<PhotoHolder>() {
+            @Override
+            public void onThumbnailDownloader(PhotoHolder target, Bitmap thumbnail) {
+                Drawable drawable = new BitmapDrawable(getResources(), thumbnail);
+                target.bindDrawable(drawable);
+            }
+        });
+        mThumbnailDownloader.start();
+        mThumbnailDownloader.getLooper();
+        Log.i(TAG, "Background thread start");
     }
 
     @Nullable
@@ -32,8 +69,169 @@ public class PhotoGalleryFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
         mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        mGridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+
+        setupAdapter();
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0){
+                    GridLayoutManager gridLayoutManager = (GridLayoutManager) mRecyclerView.getLayoutManager();
+                    int visibleItemCount = gridLayoutManager.getChildCount();
+                    int totalItemCount = gridLayoutManager.getItemCount();
+                    int lastPosition = gridLayoutManager.findFirstVisibleItemPosition();
+
+                    if ((visibleItemCount + lastPosition) >= totalItemCount){
+                        Log.i(TAG, "Current page:  " + mPage);
+                        updateItems();
+                    }
+                }
+
+            }
+        });
 
         return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailDownloader.quit();
+        Log.i(TAG, "Background thread destroyed");
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_photo_gallery, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "QueryTextSubmit" + query);
+                QueryPreferences.setStoredQuery(getActivity(), query);
+                searchView.clearFocus();
+                updateItems();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "QueryTextChange" + newText);
+                return false;
+            }
+        });
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = QueryPreferences.getStoredQuery(getActivity());
+                searchView.setQuery(query, false);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_clear:
+                QueryPreferences.setStoredQuery(getActivity(), null);
+                updateItems();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateItems(){
+        String query = QueryPreferences.getStoredQuery(getActivity());
+        new FetchItemsTask(query).execute();
+    }
+
+    private void setupAdapter(){
+        if (isAdded()){
+            mRecyclerView.setAdapter(new PhotoAdapter(mItems));
+            mRecyclerView.getAdapter().notifyDataSetChanged();
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder{
+        private ImageView mImageView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mImageView = (ImageView) itemView.findViewById(R.id.fragment_photo_gallery_image_view);
+        }
+
+        public void bindDrawable(Drawable drawable){
+            mImageView.setImageDrawable(drawable);
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
+        private List<GalleryItem> mItems;
+
+        public PhotoAdapter(List<GalleryItem> items) {
+            mItems = items;
+        }
+
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View v = layoutInflater.inflate(R.layout.gallery_item, parent, false);
+            return new PhotoHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+            GalleryItem galleryItem = mItems.get(position);
+            Drawable drawable = getResources().getDrawable(R.drawable.load);
+            holder.bindDrawable(drawable);
+            mThumbnailDownloader.queueThumbnail(holder,galleryItem.getUrl());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>>  {
+        private String mQuery;
+
+        public FetchItemsTask(String query){
+            mQuery = query;
+        }
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
+            if (mQuery == null){
+                return new FlickrFetcher().fetchRecentPhotos(String.valueOf(++mPage));
+            }else {
+                return new FlickrFetcher().searchPhotos(mQuery);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> galleryItems) {
+            if(mPage > 1 ){
+            mItems.addAll(galleryItems);
+            mRecyclerView.getAdapter().notifyDataSetChanged();
+            }else {
+                mItems = galleryItems;
+                setupAdapter();
+            }
+
+        }
     }
 }
